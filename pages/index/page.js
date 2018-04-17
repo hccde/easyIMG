8ae792d7e43df1de56c8bd005e52d707b6ec3b71
hccde <1076663958@qq.com>
2018-04-17 23:48:32 +0800

    init

--- pages/index/page.js
+++ pages/index/page.js
@@ -6,7 +6,6 @@ import wxapi from '../../libs/wx-api-promise/index'
 import wxUserinfoManager from '../../global/wxUserinfo'
 import tokenManager from '../../global/token'
 import openIDManager from '../../global/openID'
-import roleManager from '../../global/role'
 
 const isProfileComplete = (userinfo) => userinfo && userinfo.avatarUrl && userinfo.gender && userinfo.nickName
 
@@ -18,31 +17,10 @@ const getWXUserInfo =  () => new Promise(async (resolve) => {
   resolve(wxUserinfo)
 })
 
-const gotoCellphone = () => new Promise(async (resolve) => {
-  wx.redirectTo({
-    url: '/pages/passport/cellphone/page'
-  })
-  resolve()
-})
-
-const gotoSupplyProfile = () => new Promise(async (resolve)=> {
-  await getWXUserInfo()
-  wx.redirectTo({
-    url: '/pages/other/supply-profile/page'
-  })
-  resolve()
-})
-
-const gotoHome = () => new Promise(async (resolve) => {
-  const role = roleManager.get()
-  wx.redirectTo({
-    url: `/pages/${role}/index/page`
-  })
-  resolve()
-})
 
 Page({
   onLoad: async function () {
+    return; //todo
     // 校验本地token
     let token = tokenManager.get()
     // init(token)
@@ -58,7 +36,6 @@ Page({
 
     // 未登录
     if (!userinfo) {
-      return;
       // 获取微信code
       const wxLoginRes = await wxapi.login()
       console.log('wxLoginRes:', wxLoginRes)
@@ -79,33 +56,20 @@ Page({
       }
 
       token = loginRes.content.jwtToken
-      if (token) {
-        // 登录成功, 该微信用户之前已绑定openid-userid
-        tokenManager.set(token)
-        // 登录成功
-        userinfo = loginRes.content
-        // 个人信息不完整
-        if (isProfileComplete(userinfo)) {
-          await gotoHome()
+        if (!token) {
+            // 该微信用户之前未绑定opneid-userid
+            const openID = loginRes.content.openId
+            openIDManager.set(openID)
+
+            await getWXUserInfo()
         } else {
-          await gotoSupplyProfile()
+            // 登录成功, 该微信用户之前已绑定openid-userid
+            tokenManager.set(token)
+            // 登录成功
+            userinfo = loginRes.content
+            // 个人信息不完整
         }
-      } else {
-        // 该微信用户之前未绑定opneid-userid
-        const openID = loginRes.content.openId
-        openIDManager.set(openID)
-
-        await getWXUserInfo()
-        await gotoCellphone()
-      }
-      return
-    }
-
-    // 个人信息不完整
-    if (isProfileComplete(userinfo)) {
-      await gotoHome()
-    } else {
-      await gotoSupplyProfile()
+        return
     }
   }
 })
