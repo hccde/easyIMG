97e2cf8d869a7879e51948afec3cb8189d206628
hccde <1076663958@qq.com>
2018-04-23 23:52:47 +0800

    img

--- pages/platform/page.js
+++ pages/platform/page.js
@@ -4,5 +4,13 @@ Page({
     },
     onLoad(){
         console.log('onload')
-    }
+    },
+    onReady(){
+        const context = wx.createCanvasContext('canvas');
+        console.log(context)
+    },
+    canvasIdErrorCallback(e) {
+        //dont support canvas
+        console.error(e.detail.errMsg)
+    },
 })
