8f5cb518d6b6b0a05c43351e9cf716d64ead261a
admos_han <hancongcong@zhongan.io>
2018-04-27 14:50:58 +0800

    move container

--- components/move-container/com.js
+++ components/move-container/com.js
@@ -1,10 +1,36 @@
 Component({
     properties: {
-    },
-    data: {
-        x:0,
-        y:0
+        container: {
+           type:Object,
+           value:{
+            path: '',
+            width: 0,
+            height: 0
+           }
+        },
+        items:{
+            type:Array,
+            value:[{
+                path: '',
+                width: 60,
+                height: 60,
+                x: 0,
+                y: 0
+            }, {
+                path: '',
+                width: 60,
+                height: 60,
+                x: 60,
+                y: 60
+            }]
+        }
     },
     methods: {
+        scale(e){
+            console.log('scale',e);
+        },
+        move(e){
+            console.log('move',e)
+        }
     }
-  })
+})
